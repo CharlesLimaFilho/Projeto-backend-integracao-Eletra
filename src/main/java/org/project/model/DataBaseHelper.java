c738f1b8974b977d939a55e314fd75a8677f5a97
Charles Lima <charles.lima.filho17@gmail.com>
2026-03-20 11:26:20 -0300

    ### Updated: - Removed unnecessary methods from DataBaseHelper

--- src/main/java/org/project/model/DataBaseHelper.java
+++ src/main/java/org/project/model/DataBaseHelper.java
@@ -20,30 +20,6 @@ public class DataBaseHelper {
         return session = sessionFactory.openSession();
     }
 
-//    public List<LineTable> getLines() {
-//        lines = session.createQuery("from LineTable l").getResultList();
-//        return lines;
-//    }
-//
-//    public List<CategoryTable> getCategories() {
-//        categories = session.createQuery("from CategoryTable c").getResultList();
-//        return categories;
-//    }
-//
-//    public List<ModelTable> getModels() {
-//        models = session.createQuery("from ModelTable m").getResultList();
-//        return models;
-//    }
-//
-//    public List<CategoryTable> getCategoriesByLineId(int lineId) {
-//        return session.createQuery("from CategoryTable c where c.id_line =:lineId").setParameter("lineId", lineId).getResultList();
-//    }
-//
-//    public List<ModelTable> getModelsByCategoryId(int categoryId) {
-//        return session.createQuery("from ModelTable m where m.id_category =:categoryId").setParameter("categoryId",
-//                categoryId).getResultList();
-//    }
-
     public void closeConnection() {
         session.close();
         sessionFactory.close();
